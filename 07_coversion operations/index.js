485f077c8e8a6297cc50c60c4b817e13c96d46d6
Ravirohra88 <jalna.ravisr2802@gmail.com>
2025-01-15 17:22:33 +0530

    conversion operation

---
+++ 07_coversion operations/index.js
@@ -0,0 +1,46 @@
+//******************operation********************
+// let value = 6
+// let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+1); addition 
+// console.log(2-1); substraction
+// console.log(2/3);division
+// console.log(2**4);raise to power
+// console.log(2%2); remainder
+ 
+// addition  of String
+// let ravi1 = "ravi"
+// let ravi2 = " rohra"
+// let ravi = ravi1 + ravi2;
+// console.log(ravi);
+
+// conversion of diferent datatype 
+// console.log("1" + 2); 
+// console.log( 1 + "2");
+// console.log(1+4+"6");
+// console.log("6" +2+ 3);
+
+// note : it wii take the string and not do any conversion
+
+
+//  Boolean
+// console.log( +true); value 1
+// console.log( +"") value 0 
+
+// let episodeNumber = 1075
+// ++episodeNumber;
+// console.log(episodeNumber);
+
+// let episodeNumber = 1075
+// episodeNumber++;
+// console.log(episodeNumber);
+
+
+
+
+
+
+
+
