d7b9638fdf4ad01a04495a0d4de3ba53aa5cec47
Ravirohra88 <jalna.ravisr2802@gmail.com>
2025-01-16 00:50:50 +0530

    summary and memory types

---
+++ 09_Dataatpyes of javascript and  summary/index.js
@@ -0,0 +1,40 @@
+//  primative datatypes
+
+
+// 1. string
+let str1 = "ravi"
+
+// 2. number
+let a = 111
+
+// 3. boolean
+let locked = true
+
+// 4. null
+let temperature = null
+
+// 5.undefined
+let name;
+
+// 6.symbol
+const id = Symbol('$')
+
+//big int 
+const biggestnumber  = 123456789123456789n
+
+// non primitive
+
+// 1. array
+const tmkoc = ["hathi","jethalal",1];
+
+
+// 2.object
+let obj = {
+    name:"ravi",
+    age: 18
+}
+// 3. function
+let firstfunction = function () {
+    console.log("hello world")
+}
+
---
+++ 10_stack and heap memory/index.js
@@ -0,0 +1,18 @@
+// stack memory
+let myinsta = "ravisr_028"
+let anothername = myinsta
+anothername =" badepappaji"
+console.log(anothername);
+console.log(myinsta);
+
+
+// heap memory
+let ravi = {
+    name:"ravi",
+    marks:100,
+}
+let nakul = ravi 
+nakul.name = "nakul"
+console.log(ravi);
+console.log(nakul);
+
