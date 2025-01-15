0b94e3c7e94c1014d1e95de25b28799c7422df34
Ravirohra88 <jalna.ravisr2802@gmail.com>
2025-01-15 20:12:45 +0530

    COMPARISION

---
+++ 08_comparision/index.js
@@ -0,0 +1,20 @@
+// console.log( 3 > 1);
+// console.log( 3 < 1);
+// console.log( 3 == 1);
+// console.log( 3 != 1);
+// console.log( 3 >= 1);
+
+
+console.log( undefined > 1);
+console.log( undefined < 1);
+console.log( undefined == 1);
+console.log( null >=1 );
+console.log( null ==0 );
+console.log( null <=0 );
+
+
+console.log( null > 1);
+console.log( "ravi"> 1);
+console.log( true > 1);
+console.log( "1" == 1);
+console.log( "1" === 1);
