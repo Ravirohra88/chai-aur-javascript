e4037c9e293bd42c91f0f2aa50320787b1fe5ff0
Ravirohra88 <jalna.ravisr2802@gmail.com>
2025-01-16 20:37:15 +0530

    math and numbers

---
+++ 12_number and maths/num.js
@@ -0,0 +1,19 @@
+let marks = 99
+console.log(marks);
+
+let money = new Number(10000)
+console.log(money);
+
+
+console.log(money.toString().length);
+console.log(money.toFixed(2));
+
+let number = 1233.233
+console.log(number.toPrecision(5));
+
+let maxnum = 1000000000
+console.log(maxnum.toLocaleString('en-In'));
+
+
+
+                
